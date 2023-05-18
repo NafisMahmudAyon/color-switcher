a946fd67be8aa066dbcd2d1f3e7f83c4a509e870
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-05-18 14:31:04 +0600

    Color Load More Button Add

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,13 @@ export default function RootLayout({ children }) {
       </Head>
       <body className="">
         {/* Navbar  */}
-        <nav className="h-14 flex justify-center items-center text-emerald-500 font bold text-4xl"> Color Palett Generator </nav>
+        <nav className="h-14 flex justify-between items-center  px-20"> 
+          <div className="text-4xl text-emerald-500 font-bold hover:underline">Color Palette Generator</div>
+          <div className="flex gap-3">
+            <div className="bg-emerald-500 px-5 py-2 rounded-lg hover:bg-emerald-800 hover:text-white font-bold"> <a href="https://github.com/NafisMahmudAyon">github</a> </div>
+            <div className="bg-emerald-500 px-5 py-2 rounded-lg hover:bg-emerald-800 hover:text-white font-bold"> <a href="https://nafisbd.com"> Visit My Website</a></div>
+          </div>
+        </nav>
         {/* main */}
         {children}
         
